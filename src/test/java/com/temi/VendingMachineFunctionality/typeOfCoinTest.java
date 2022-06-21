1bfabf6df3e713748360521f48217fb4e4a38924
Temitope Jegede <temiteejegede@gmail.com>
2022-06-20 23:47:10 -0400

    Correct tests still to be written

--- src/test/java/com/temi/VendingMachineFunctionality/typeOfCoinTest.java
+++ src/test/java/com/temi/VendingMachineFunctionality/typeOfCoinTest.java
@@ -1,5 +1,23 @@
 package com.temi.VendingMachineFunctionality;
 
+import com.temi.coins.Coin;
+
 public class typeOfCoinTest {
 
+    private void testTypeOfCoin__PENNY(){
+        Coin myCoin = Coin.PENNY;
+
+    }
+
+    private void testTypeOfCoin__NICKEL(){
+
+    }
+
+    private void testTypeOfCoin__DIME(){
+
+    }
+
+    private void testTypeOfCoin__QUARTER(){
+
+    }
 }
