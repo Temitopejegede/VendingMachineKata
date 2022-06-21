3104a61ea239c81114652abb854574ec9026b5f3
Temitope Jegede <temiteejegede@gmail.com>
2022-06-21 13:13:48 -0400

    Tests to make sure the vending machine can identify coins

---
+++ src/test/java/com/temi/VendingMachineFunctionality/TypeOfCoinTest.java
@@ -0,0 +1,39 @@
+package com.temi.VendingMachineFunctionality;
+
+import com.temi.coins.Coin;
+import com.temi.vendingMachineFunctionality.VendingMachine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TypeOfCoinTest {
+
+    @Test
+     void testTypeOfCoinWhenExpectedCoinIsPenny(){
+        Coin inputCoin = Coin.PENNY;
+        VendingMachine myMachine = new VendingMachine();
+        Coin expectedCoin = myMachine.measureCoinDimensions(inputCoin);
+        assertEquals(Coin.PENNY, expectedCoin);
+    }
+
+    private void testTypeOfCoinWhenExpectedCoinIsDime(){
+        Coin inputCoin = Coin.DIME;
+        VendingMachine myMachine = new VendingMachine();
+        Coin expectedCoin = myMachine.measureCoinDimensions(inputCoin);
+        assertEquals(Coin.PENNY, expectedCoin);
+    }
+
+    private void testTypeOfCoinWhenExpectedCoinIsNickel(){
+        Coin inputCoin = Coin.NICKEL;
+        VendingMachine myMachine = new VendingMachine();
+        Coin expectedCoin = myMachine.measureCoinDimensions(inputCoin);
+        assertEquals(Coin.PENNY, expectedCoin);
+    }
+
+    private void testTypeOfCoinWhenExpectedCoinIsQuarter(){
+        Coin inputCoin = Coin.QUARTER;
+        VendingMachine myMachine = new VendingMachine();
+        Coin expectedCoin = myMachine.measureCoinDimensions(inputCoin);
+        assertEquals(Coin.PENNY, expectedCoin);
+    }
+}
