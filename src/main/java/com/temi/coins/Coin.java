bdea15b72a2618799a7fd455c031a372a1d3febc
Temitope Jegede <temiteejegede@gmail.com>
2022-06-20 23:48:28 -0400

    HashMap could be used to map coin type to value

--- src/main/java/com/temi/coins/Coin.java
+++ src/main/java/com/temi/coins/Coin.java
@@ -1,22 +1,53 @@
 package com.temi.coins;
 
 public enum Coin {
-    PENNY(2.5, 19.05, 1.52, "plain", 0),
-    NICKEL(5.0, 21.21, 1.95, "plain", 0),
-    DIME(2.268, 17.91, 1.35, "reeded", 118),
-    QUARTER(5.670, 24.26, 1.75, "reeded", 119);
+    PENNY("2.5", "19.05", "1.52", "plain", "0"),
+    NICKEL("5.0", "21.21", "1.95", "plain", "0"),
+    DIME("2.268", "17.91", "1.35", "reeded", "118"),
+    QUARTER("5.670", "24.26", "1.75", "reeded", "119");
 
-    private final double WEIGHT;
-    private final double DIAMETER;
-    private final double THICKNESS;
+    private final String WEIGHT;
+    private final String DIAMETER;
+    private final String THICKNESS;
     private final String EDGE;
-    private final int NUMBEROFREEDS;
+    private final String NUMBEROFREEDS;
 
-    Coin(double WEIGHT, double DIAMETER, double THICKNESS, String EDGE, int NUMBEROFREEDS){
+    Coin(String WEIGHT, String DIAMETER, String THICKNESS, String EDGE, String NUMBEROFREEDS){
         this.WEIGHT = WEIGHT;
         this.DIAMETER = DIAMETER;
         this.THICKNESS = THICKNESS;
         this.EDGE = EDGE;
         this.NUMBEROFREEDS = NUMBEROFREEDS;
+    }
+
+    public String getWEIGHT() {
+        return WEIGHT;
+    }
+
+    public String getDIAMETER() {
+        return DIAMETER;
+    }
+
+    public String getTHICKNESS() {
+        return THICKNESS;
+    }
+
+    public String getEDGE() {
+        return EDGE;
+    }
+
+    public String getNUMBEROFREEDS() {
+        return NUMBEROFREEDS;
+    }
+
+    @Override
+    public String toString() {
+        return "Coin{" +
+                "WEIGHT=" + WEIGHT +
+                ", DIAMETER=" + DIAMETER +
+                ", THICKNESS=" + THICKNESS +
+                ", EDGE='" + EDGE + '\'' +
+                ", NUMBEROFREEDS=" + NUMBEROFREEDS +
+                '}';
     }
 }
--- src/main/java/com/temi/vendingMachineFunctionality/VendingMachine.java
+++ src/main/java/com/temi/vendingMachineFunctionality/VendingMachine.java
@@ -2,6 +2,36 @@ package com.temi.vendingMachineFunctionality;
 
 import com.temi.coins.*;
 
-public class VendingMachine {
+import java.util.HashMap;
+import java.util.Map;
 
+public class VendingMachine {
+    private static final double PENNYVALUE = 0.01;
+    private static final double NICKELVALUE = 0.05;
+    private static final double DIMEVALUE = 0.10;
+    private static final double QUARTERVALUE = 0.25;
+
+    HashMap<String, Integer> coinValueMap = new HashMap<>();
+    public double getCoinValue(Coin myCoin){
+        if(myCoin.getEDGE().equals("plain")){
+            if(myCoin.getWEIGHT().equals("2.5")
+                    && myCoin.getDIAMETER().equals("19.05")
+                    && myCoin.getTHICKNESS().equals("1.52")
+                    && myCoin.getNUMBEROFREEDS().equals("0")) return PENNYVALUE;
+            if(myCoin.getWEIGHT().equals("5.0")
+                    && myCoin.getDIAMETER().equals("21.21")
+                    && myCoin.getTHICKNESS().equals("1.95")
+                    && myCoin.getNUMBEROFREEDS().equals("0")) return NICKELVALUE;
+        } else{
+            if(myCoin.getWEIGHT().equals("2.268")
+                    && myCoin.getDIAMETER().equals("17.91")
+                    && myCoin.getTHICKNESS().equals("1.35")
+                    && myCoin.getNUMBEROFREEDS().equals("118")) return DIMEVALUE;
+            if(myCoin.getWEIGHT().equals("5.670")
+                    && myCoin.getDIAMETER().equals("24.26")
+                    && myCoin.getTHICKNESS().equals("1.75")
+                    && myCoin.getNUMBEROFREEDS().equals("119")) return QUARTERVALUE;
+        }
+        return 0;
+    }
 }
