5a5e158e16cdba2b6b7063f3d4e8ceacbb4126cd
Temitope Jegede <temiteejegede@gmail.com>
2022-06-21 18:44:58 -0400

    test if vending machine assigns correct coin value

--- src/test/java/com/temi/VendingMachineFunctionality/TypeOfCoinTests.java
+++ src/test/java/com/temi/VendingMachineFunctionality/TypeOfCoinTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TypeOfCoinTest {
+public class TypeOfCoinTests {
 
     @Test
      void testTypeOfCoinWhenExpectedCoinIsPenny(){
---
+++ src/test/java/com/temi/VendingMachineFunctionality/ValueOfCoinTests.java
@@ -0,0 +1,27 @@
+package com.temi.VendingMachineFunctionality;
+
+import com.temi.coins.*;
+import org.junit.jupiter.api.Test;
+
+public class ValueOfCoinTests {
+
+    @Test
+    void testValueOfCoinWhenCoinIsPenny(){
+
+    }
+
+    @Test
+    void testValueOfCoinWhenCoinIsDIME(){
+
+    }
+
+    @Test
+    void testValueOfCoinWhenCoinIsNICKEL(){
+
+    }
+
+    @Test
+    void testValueOfCoinWhenCoinIsQUARTER(){
+
+    }
+}
