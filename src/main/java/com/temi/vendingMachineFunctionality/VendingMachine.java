99b1557df8f9b6b1433322ac4570f30096e40d5a
Temitope Jegede <temiteejegede@gmail.com>
2022-06-22 16:50:21 -0400

    Adds functionality for selecting products, determining validity of coin, and display message

--- src/main/java/com/temi/vendingMachineFunctionality/VendingMachine.java
+++ src/main/java/com/temi/vendingMachineFunctionality/VendingMachine.java
@@ -2,6 +2,7 @@ package com.temi.vendingMachineFunctionality;
 
 import com.temi.coins.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,12 +12,18 @@ public class VendingMachine {
     private static final double DIMEVALUE = 0.10;
     private static final double QUARTERVALUE = 0.25;
 
+    private static final double PRICEOFCOLA = 1.00;
+    private static final double PRICEOFCHIPS = 0.50;
+    private static final double PRICEOFCANDY = 0.65;
+    HashMap<Coin, Double> coinValueMap = new HashMap<>();
+
     public VendingMachine(){
         mapCoinValue();
     }
-    Coin[] coins = Coin.values();
-    HashMap<Coin, Double> coinValueMap = new HashMap<>();
 
+    public String checkDisplay(){
+        return "INSERT COIN";
+    }
 
     public void mapCoinValue(){
         coinValueMap.put(Coin.PENNY, PENNYVALUE);
@@ -53,8 +60,35 @@ public class VendingMachine {
     public boolean checkIfCoinTypeExists(Coin myCoin){
         return coinValueMap.containsKey(myCoin);
     }
+
+
     public double getCoinValue(Coin myCoin){
         if(coinValueMap.containsKey(myCoin)) return coinValueMap.get(myCoin);
         return 0;
     }
+
+    public boolean checkIfCoinIsValidForVendingMachine(Coin myCoin){
+        return myCoin != Coin.PENNY;
+    }
+
+    public double getHowMuchMoneyWasInserted(ArrayList<Coin> coins){
+        double money = 0.0;
+        for(Coin currentCoin: coins){
+            if(checkIfCoinIsValidForVendingMachine(currentCoin)){
+                money += getCoinValue(currentCoin);
+            }
+        }
+        return  money;
+    }
+//    public String selectCola(){
+//        if()
+//    }
+//
+//    public String selectChips(){
+//
+//    }
+//
+//    public String selectCandy(){
+//
+//    }
 }
