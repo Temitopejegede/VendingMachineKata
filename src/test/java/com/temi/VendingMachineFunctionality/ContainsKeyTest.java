200df987cbceac44eb48938ad3522786a1894156
Temitope Jegede <temiteejegede@gmail.com>
2022-06-21 22:05:11 -0400

    Tests to make sure hashmap returned the right values

---
+++ src/test/java/com/temi/VendingMachineFunctionality/ContainsKeyTest.java
@@ -0,0 +1,38 @@
+package com.temi.VendingMachineFunctionality;
+
+import com.temi.coins.Coin;
+import com.temi.vendingMachineFunctionality.VendingMachine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContainsKeyTest {
+
+    @Test
+    void containsKeyWhenKeyIsPennyTest(){
+        Coin myCoin = Coin.PENNY;
+        VendingMachine myMachine = new VendingMachine();
+        assertTrue(myMachine.checkIfCoinTypeExists(myCoin));
+    }
+
+    @Test
+    void containsKeyWhenKeyIsDimeTest(){
+        Coin myCoin = Coin.DIME;
+        VendingMachine myMachine = new VendingMachine();
+        assertTrue(myMachine.checkIfCoinTypeExists(myCoin));
+    }
+
+    @Test
+    void containsKeyWhenKeyIsNickelTest(){
+        Coin myCoin = Coin.NICKEL;
+        VendingMachine myMachine = new VendingMachine();
+        assertTrue(myMachine.checkIfCoinTypeExists(myCoin));
+    }
+
+    @Test
+    void containsKeyWhenKeyIsQuarterTest(){
+        Coin myCoin = Coin.QUARTER;
+        VendingMachine myMachine = new VendingMachine();
+        assertTrue(myMachine.checkIfCoinTypeExists(myCoin));
+    }
+}
