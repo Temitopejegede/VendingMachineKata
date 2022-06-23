11be5060ad6190ba1e72d883568c2ed754c408d1
Temitope Jegede <temiteejegede@gmail.com>
2022-06-23 16:31:16 -0400

    Test random coin generation

--- src/test/java/com/temi/VendingMachineFunctionality/GenerateRandomInputTests.java
+++ src/test/java/com/temi/VendingMachineFunctionality/GenerateRandomInputTests.java
@@ -1,4 +1,27 @@
 package com.temi.VendingMachineFunctionality;
 
+import com.temi.coins.Coin;
+import com.temi.inputs.Inputs;
+import com.temi.vendingMachineFunctionality.VendingMachine;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GenerateRandomInputTests {
+
+    @Test
+    void testGenerateRandomCoin(){
+        Inputs myInputs = new Inputs();
+        ArrayList<Coin> testInput = myInputs.generateRandomCoins();
+        assertTrue(testInput.size() >=5 && testInput.size() <= 7);
+
+    }
+    @Test
+    void testGenerateOnlyValidCoins(){
+        Inputs myInputs = new Inputs();
+        ArrayList<Coin> testInput = myInputs.generateOnlyValidCoins();
+        assertFalse(testInput.contains(Coin.PENNY));
+    }
 }
