bc3e2f10e93a9cfa043f8cda7ccf1c38b0fb3fd7
Temitope Jegede <temiteejegede@gmail.com>
2022-06-27 23:43:16 -0400

    new test class

---
+++ src/test/java/com/temi/VendingMachineFunctionality/OutOfStockItemTests.java
@@ -0,0 +1,11 @@
+package com.temi.VendingMachineFunctionality;
+
+import org.junit.jupiter.api.Test;
+
+public class OutOfStockItemTests {
+
+    @Test
+    void  GetAnItemWhenItsOutOfStockTest(){
+
+    }
+}
