4452c533ad2a5f3e43c4593420c29cc5cf05d6a1
Temitope Jegede <temiteejegede@gmail.com>
2022-06-27 22:47:28 -0400

    Third test is failing. Trying to figure out why

--- src/test/java/com/temi/VendingMachineFunctionality/returnChange/ReturnChangeAmountTests.java
+++ src/test/java/com/temi/VendingMachineFunctionality/returnChange/ReturnChangeAmountTests.java
@@ -87,26 +87,6 @@ public class ReturnChangeAmountTests {
         assertEquals(0.25, change);
     }
 
-//    @Test
-//    void testReturnChangeNumberSecondTest(){
-//        ArrayList<Coin> money = new ArrayList<>(); //$1.45
-//        money.add(Coin.QUARTER);
-//        money.add(Coin.QUARTER);
-//        money.add(Coin.QUARTER);
-//        money.add(Coin.QUARTER);
-//        money.add(Coin.QUARTER);
-//        money.add(Coin.QUARTER);
-//        money.add(Coin.NICKEL); //0.05
-//        money.add(Coin.DIME); //0.10
-//        money.add(Coin.NICKEL);
-//        VendingMachine myMachine = new VendingMachine();
-//        ArrayList<Coin> userInput = myMachine.inputCoins(money);
-//        double inputMoney = myMachine.getHowMuchMoneyWasInserted(userInput);
-//        myMachine.selectItem(Item.COLA);
-//        double change = myMachine.returnChangeNumber();
-//        assertEquals(0.70, Math.round(change*100)/100D);
-//    }
-
     @Test
     void testReturnChangeTest1(){
         ArrayList<Coin> money = new ArrayList<>(); //$1.70
@@ -148,7 +128,7 @@ public class ReturnChangeAmountTests {
         ArrayList<Coin> userInput = myMachine.inputCoins(money);
         double inputMoney = myMachine.getHowMuchMoneyWasInserted(userInput);
         myMachine.selectItem(Item.COLA);
-        ArrayList<String> change = myMachine.returnChangeInCoins(myMachine.getMoney());
+        ArrayList<String> change = myMachine.returnChangeInCoins();
         ArrayList<String> answer = new ArrayList<>();
         answer.add(Coin.QUARTER.getNAME());
         answer.add(Coin.QUARTER.getNAME());
